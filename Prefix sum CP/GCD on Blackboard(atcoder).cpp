8ac428a1e60190b08094e4b258db9d4f1aa90bd4
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-08-30 21:44:54 +0530

    update

---
+++ Prefix sum CP/GCD on Blackboard(atcoder).cpp
@@ -0,0 +1,102 @@
+// https://atcoder.jp/contests/abc125/tasks/abc125_c
+
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+#define MOD 1000000007
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define all(x) (x).begin(), (x).end()
+#define debug(x) cerr << x << " ";
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //clock_t time_req = clock();
+
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) 
+    {
+        cin >> v[i];
+    }
+    //7 6 8
+    // creating prefix gcd 
+    vector<int> prefix(n);
+    for (int i = 0; i < n; i++)
+    {
+        if (i == 0)
+        {
+            prefix[i] = v[i];
+        }
+        else
+        {
+        	int a=prefix[i-1];
+        	int b=v[i];
+            prefix[i] = gcd(a, b);
+
+        }
+    }
+    //7 1 1
+    //crating sufix gcd array
+    vector<int>prefix2(n);
+    for(int i=n-1;i>=0;i--)
+    {
+    	if(i== n-1)
+    	{
+    		prefix2[i]=v[i];
+    	}
+    	else
+    	{
+    		prefix2[i]=gcd(prefix2[i+1],v[i]);
+    	}
+    }
+    //1 2 8
+    //handling the mid element case in 3 elements 
+    int temp=gcd(prefix2[n-1],prefix[0]);
+
+    //calculating max gcd in prefix
+    int max1=0;
+    for(int i=1;i<n;i++)
+    {
+        max1=max(max1,prefix[i]);
+    }
+    //calculating max in sufix array
+    // int max2=0;
+    for(int i=0;i<n-1;i++)
+    {
+        max1=max(max1,prefix2[i]);
+    }
+    //max in both prefix and suffix it the max ans
+    int ans=max(temp,max1);
+    cout<<ans<<endl;
+
+    int ans = 0 ;
+    
+    for(int i = 0 ; i < n ;i++){
+      
+      if(i ==0) ans = max(ans , prefix2[i+1]);
+      else if (i==n-1) ans = max(ans , prefix[i-1]);
+      else {
+        ans = max(ans , gcd(prefix[i-1] , prefix2[i+1]));
+      }
+      
+    }
+
+}
