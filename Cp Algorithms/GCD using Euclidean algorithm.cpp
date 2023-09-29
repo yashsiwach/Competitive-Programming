56d330015994188bc7c472646903344772be69dd
yashsiwach <yash.12207847@lpu.in>
2023-09-29 23:32:52 +0530

    update

--- Cp Algorithms/GCD using Euclidean algorithm.cpp
+++ Cp Algorithms/GCD using Euclidean algorithm.cpp
@@ -1,3 +1,11 @@
+/*
+--------------GCD using Euclidean Algorithm---------------
+
+Time complexity :O(log min(a,b));
+
+
+
+*/
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -10,19 +18,26 @@ typedef long long ll;
 #define set_bits __builtin_popcountll
 #define all(x) (x).begin(), (x).end()
 #define debug(x) cerr<<x<<" ";
-
+int gcdline(int a,int b)
+{
+    return b ? gcdline(b,a%b) : a;
+}
 int gcd(int a,int b)
 {
-	if(b==0) return b;
-	else return gcd(b,a%b);
+    //if dividend is 0 then return a;
+    if(b==0) return a;
+    //Now divisor will become divident and reminder will becomes divisor  
+    else return gcd(b,a%b);
 }
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-	
-	int a ,b;
-	cout<<gcd(a,b)<<endl;    
+    
+    int a ,b;
+    cin>>a>>b;
+    cout<<gcd(a,b)<<endl;  
+    cout<<gcdline(a,b)<<endl;  
 }
 
